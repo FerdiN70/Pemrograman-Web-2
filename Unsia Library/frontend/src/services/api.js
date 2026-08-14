fd13f9cb816bf338c3045351b958cceb024b20c3
Ferdi <ferdi.napit02@gmail.com>
2026-08-14 20:21:27 +0700

    fix error auth login

--- Unsia Library/frontend/src/services/api.js
+++ Unsia Library/frontend/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+    baseURL: `${import.meta.env.VITE_API_URL || 'http://localhost:5000'}/api`
 });
+
 // Otomatis menyertakan token JWT di setiap request jika user sudah login
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem('token');
